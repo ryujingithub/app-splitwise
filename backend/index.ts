fac66d789ad7d9e27b59b80d0420a9b15dcf5976
Ryujin Github <ryujin.me+github@gmail.com>
2026-01-09 22:25:06 +0800

    added wrangler and added convex as backend dependency

--- backend/index.ts
+++ backend/index.ts
@@ -6,20 +6,38 @@ import { bills } from "./routes/bills";
 import { groups } from "./routes/groups";
 import { ocr } from "./routes/ocr";
 import { aiModels } from "./routes/ai-models";
+import type { Fetcher } from "@cloudflare/workers-types";
+import { HonoWithConvex, HttpRouterWithHono } from "convex-helpers/server/hono";
+import { ActionCtx } from "../convex/_generated/server";
 
-const app = new Hono();
+type Bindings = {
+    CONVEX_URL: string;
+    ASSETS: Fetcher;
+};
 
+const app: HonoWithConvex<ActionCtx> = new Hono();
+// 3. CORS Configuration
 app.use(
-    "/*",
+    "/api/*",
     cors({
+        // In production, you might want to change this to your actual domain
+        // or just keep it * if you are comfortable with that.
         origin: "http://localhost:5173",
     })
 );
 
-app.route("/api/users", users);
-app.route("/api/groups", groups);
-app.route("/api/bills", bills);
-app.route("/api/ocr", ocr);
-app.route("/api/models", aiModels);
+// app.get("/listMessages/:userId{[0-9]+}", async (c) => {
+//     const userId = c.req.param("userId");
+//     const messages = await c.env.runQuery(api.messages.getByAuthor, {
+//         authorNumber: userId,
+//     });
+//     return c.json(messages);
+// });
+
+app.route("/users", users);
+app.route("/groups", groups);
+app.route("/bills", bills);
+app.route("/ocr", ocr);
+app.route("/models", aiModels);
 
 serve({ fetch: app.fetch, port: 3001 });
