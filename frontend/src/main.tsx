8decadfdb82ee55ed06566412ac37ef4454fae08
Ryujin Github <ryujin.me+github@gmail.com>
2026-01-18 15:13:47 +0800

    remove test api in main component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./auth/auth-context";
 
 import "./index.css";
-import { TestApi } from "./features/test-connection/test-api";
 
 const client = new QueryClient();
 
@@ -14,7 +13,6 @@ createRoot(document.getElementById("root")!).render(
         <AuthProvider>
             <QueryClientProvider client={client}>
                 <App />
-                <TestApi />
             </QueryClientProvider>
         </AuthProvider>
     </StrictMode>,
